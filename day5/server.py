2e2b2bfc5d0a69adec564734a85568fae5070fa7
Amandine Underwood <amandineunderwood@MacBook-Air-dAmandine.local>
2024-09-20 12:27:21 +0200

    add run.sh and index.html and requirements.txt and server.py

--- day5/server.py
+++ day5/server.py
@@ -1,5 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+)
+
 from fastapi.responses import HTMLResponse
 @app.get("/", response_class=HTMLResponse)
 def get_index():
   with open('index.html', 'r') as file:
-    return file.read() 
+    return file.read()
